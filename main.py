4437093c75a5619739c9a0ba62143b5f788f6db2
tdhuynh <tommyhuynh93@gmail.com>
2017-01-27 15:47:52 -0500

    trying to return valid words

--- main.py
+++ main.py
@@ -5,5 +5,16 @@ with open('sowpods.txt') as f:
     word_list = f.read().split('\n')
 
 parser = argparse.ArgumentParser(description="Letter Rack Reader")
-parser.add_argument('rack', type=str, help='letters in the Scrabble rack')
+parser.add_argument('rack', type=str, help='The letters in your scrabble rack -NO SPACES, all upper or all lower.')
 rack = parser.parse_args().rack.upper()
+
+rack_list = [x for x in rack]
+print(rack_list)
+
+valid = []
+for word in word_list:
+    for x in word:
+        if x not in rack_list:
+            continue
+        else:
+            valid.append(x)
