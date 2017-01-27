a6fa2f64774816641441f5ccc01ef3b44d5de44a
tdhuynh <tommyhuynh93@gmail.com>
2017-01-27 14:52:09 -0500

    letters are in the rack

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import sys
+import argparse
 
 with open('sowpods.txt') as f:
     word_list = f.read().split('\n')
 
-scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
-         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
-         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
-         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
-         "x": 8, "z": 10}
+parser = argparse.ArgumentParser(description="Letter Rack Reader")
+parser.add_argument('rack', type=str, help='letters in the Scrabble rack')
+rack = parser.parse_args().rack.upper()
